a20b054523bd3b8d13136fef7cb091fa1fafed85
k2tzumi <katzumi+github@gmail.com>
2023-10-29 19:42:48 +0900

    Update slack client type

--- src/slack/types/callback-events.d.ts
+++ src/slack/types/callback-events.d.ts
@@ -17,5 +17,28 @@ declare namespace Slack {
       text: string;
       channel: string;
     }
+    interface MessageEvent extends EventBase {
+      channel: string;
+      text?: string;
+      subtype?: string;
+      thread_ts?: string;
+      bot_id?: string;
+    }
+    interface MessageRepliedEvent extends MessageEvent {
+      message?: {
+        type: string;
+        user: string;
+        text: string;
+        thread_ts: string;
+        reply_count: number;
+        replies: {
+          user: string;
+          ts: string;
+        }[];
+        ts: string;
+      };
+      subtype: string;
+      hidden: boolean;
+    }
   }
 }
