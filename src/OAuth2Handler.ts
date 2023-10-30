967031a0d926e7b846747bbdb185fba184686fc2
k2tzumi <katzumi+github@gmail.com>
2023-10-30 21:13:05 +0900

    Fixing `missing_scope`

--- src/OAuth2Handler.ts
+++ src/OAuth2Handler.ts
@@ -109,7 +109,7 @@ class OAuth2Handler {
   }
 
   public static readonly SCOPE =
-    "commands,chat:write,reactions:write,app_mentions:read";
+    "commands,chat:write,reactions:write,app_mentions:read,channels:write,groups:write,mpim:write,im:write";
 
   private service: OAuth2Service;
 
